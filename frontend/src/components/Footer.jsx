f682a10868511d66a10319f341d4774311fdfffb
Ranjithkarthi <81366510+Ranjithkarthi@users.noreply.github.com>
2024-10-31 02:36:46 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div>
             <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
                 <div>
-                    <img src={assets.logo} alt="" className='mb-5 w-32' />
+                    <img src={assets.logo} alt="brand logo" className='mb-5 w-32' />
                     <p className='w-full  md:w-2/3 text-gray-600' >
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when  unknown printer took a galley of type and scrambled it to make a type specimen book.
                     </p>
@@ -24,14 +24,14 @@ const Footer = () => {
                     <p className='text-xl font-medium mb-5'>Get in touch</p>
                     <ul className='flex flex-col gap-1 text-gray-600'>
                         <li>+91 8077669812</li>
-                        <li>nomanAli50997@gmail.com</li>
+                        <li>onestopsupport@gmail.com</li>
                     </ul>
                 </div>
 
             </div>
             <div>
                 <hr />
-                <p className='py-5 text-sm text-center'>Copyright 2024 &copy; forever.com - All Right Reserved</p>
+                <p className='py-5 text-sm text-center'>Copyright 2024 &copy; onestop.com - All Right Reserved</p>
             </div>
         </div>
 
